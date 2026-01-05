cacb62eb05a0db0f3ccab3099e8eb438cba3d142
PeterVD <pvd@tenera-it.be>
2026-01-05 16:21:39 +0100

    Add basic Avalonia project

---
+++ src/PdfChopper/PdfChopper/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PdfChopper.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ src/PdfChopper/PdfChopper/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace PdfChopper.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
